d15f4cd80eb930c4264dfc639a7f1d08ed24acfd
Yury Slabodchykau <yury.slabodchykau@gmail.com>
2023-07-09 14:43:45 +0200

    Extend the Parrot and Penguin class

--- birds.py
+++ birds.py
@@ -1,15 +1,23 @@
-class Bird():
+class Bird:
     def __init__(self, name, size):
         self.name = name
         self.size = size
 
     def describe(self):
-        return f'Размер птицы {self.name} - {self.size}.'
+        return f'Размер птицы {self.name} — {self.size}.'
 
 
 class Parrot(Bird):
-    ...
+    def __init__(self, name, size, color):
+        super().__init__(name, size)
+        self.color = color
 
 
 class Penguin(Bird):
-    ...
+    def __init__(self, name, size, genus):
+        super().__init__(name, size)
+        self.genus = genus
+
+
+kesha = Parrot('Ара', 'средний', 'красный')
+kowalski = Penguin('Королевский', 'большой', 'Aptenodytes')
