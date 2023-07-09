0c1627f2223319d997e046274d38b7f84b909793
Yury Slabodchykau <yury.slabodchykau@gmail.com>
2023-07-09 15:14:29 +0200

    Override Describe method

--- birds.py
+++ birds.py
@@ -3,7 +3,7 @@ class Bird:
         self.name = name
         self.size = size
 
-    def describe(self):
+    def describe(self, full=False):
         return f'Размер птицы {self.name} — {self.size}.'
 
 
@@ -12,12 +12,41 @@ class Parrot(Bird):
         super().__init__(name, size)
         self.color = color
 
+    def describe(self, full=False):
+        self.full = full
+        if not full:
+            return super().describe()
+        else:
+            return (f'Попугай {self.name} — заметная птица, '
+                    f'окрас её перьев — {self.color}, а размер — {self.size}. '
+                    f'Интересный факт: попугаи чувствуют ритм, '
+                    f'а вовсе не бездумно двигаются под музыку. '
+                    f'Если сменить композицию, '
+                    f'то и темп движений птицы изменится.')
+
 
 class Penguin(Bird):
     def __init__(self, name, size, genus):
         super().__init__(name, size)
         self.genus = genus
 
+    def describe(self, full=False):
+        self.full = full
+        if not full:
+            return super().describe()
+        else:
+            return (f'Размер пингвина {self.name} из рода {self.genus} — '
+                    f'{self.size}. '
+                    f'Интересный факт: однажды группа геологов-разведчиков '
+                    f'похитила пингвинье яйцо, и их принялась преследовать вся'
+                    f' стая, не пытаясь, впрочем, при этом нападать.'
+                    f' Посовещавшись, похитители вернули птицам яйцо,'
+                    f' и те отстали.')
+
 
 kesha = Parrot('Ара', 'средний', 'красный')
 kowalski = Penguin('Королевский', 'большой', 'Aptenodytes')
+
+
+print(kesha.describe())
+print(kowalski.describe(True))
